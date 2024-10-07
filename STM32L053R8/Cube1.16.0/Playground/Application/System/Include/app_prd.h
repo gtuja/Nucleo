d9e94c062ef1877702af2d33d3ed270245a34b5a
Seho Seo <hkf0105@gmail.com>
2024-10-07 23:52:33 +0900

    [L053R8_NOOS_SYSTEM] Implemented APRD basic.

--- STM32L053R8/Cube1.16.0/Playground/Application/System/Include/app_prd.h
+++ STM32L053R8/Cube1.16.0/Playground/Application/System/Include/app_prd.h
@@ -14,11 +14,28 @@
 #include "type.h"
 
 /* Exported defines ----------------------------------------------------------*/
+#define APRD_SRV_MAX          10
+#define APRD_INTERVAL_LOW     100
+#define APRD_INTERVAL_NORMAL  10
+#define APRD_INTERVAL_HIGH    1
+
 /* Exported types ------------------------------------------------------------*/
+typedef enum {
+  APRD_PRIOR_NA = 0,
+  APRD_PRIOR_LOW,
+  APRD_PRIOR_NORMAL,
+  APRD_PRIOR_HIGH,
+  APRD_PRIOR_MAX,
+} tenuAprdPriority;
+
+typedef void (*tpfAprdInitialize)(void* pvArgs);
+typedef void (*tpfAprdService)(void* pvArgs);
+
 /* Exported constants --------------------------------------------------------*/
 /* Exported functions prototypes ---------------------------------------------*/
 
 EXTERN void vidAprdInitialize(void);
 EXTERN void vidAprdService(void);
+EXTERN BOOL bAprdRegister(tenuAprdPriority enuPriority, tpfAprdInitialize pfInitialize, tpfAprdService pfService);
 
 #endif /* SYSTEM_INCLUDE_APP_PRD_H_ */
--- STM32L053R8/Cube1.16.0/Playground/Application/System/Source/app_prd.c
+++ STM32L053R8/Cube1.16.0/Playground/Application/System/Source/app_prd.c
@@ -10,29 +10,75 @@
 #include "type.h"
 #include "feature.h"
 #include "stm32l0xx_hal.h"
+#include "app_prd.h"
+#include <string.h>
 
 /* Private define ------------------------------------------------------------*/
 /* Private typedef -----------------------------------------------------------*/
+typedef struct {
+  tenuAprdPriority  enuPriority;
+  tpfAprdInitialize pfInitialize;
+  tpfAprdService    pfService;
+} tstrAprdService;
+
 /* Private macro -------------------------------------------------------------*/
 /* Private function prototypes -----------------------------------------------*/
+PRIVATE void vidAprdServiceDetail(tenuAprdPriority enuAprdPriority);
+
 /* Private variables ---------------------------------------------------------*/
+PRIVATE U64 gullAprdCounter;
+
+PRIVATE tstrAprdService gstrAprdService[APRD_SRV_MAX];
+
 /* Exported variables --------------------------------------------------------*/
 extern TIM_HandleTypeDef htim2;
 extern TIM_HandleTypeDef htim21;
 
 /* Public functions ----------------------------------------------------------*/
-
 PUBLIC void vidAprdInitialize(void) {
+  gullAprdCounter = 0;
+  memset(gstrAprdService, 0, sizeof(gstrAprdService));
 }
 
 PUBLIC void vidAprdService(void) {
+  if ((gullAprdCounter % APRD_INTERVAL_LOW) == (U64)0)    vidAprdServiceDetail(APRD_PRIOR_LOW);
+  if ((gullAprdCounter % APRD_INTERVAL_NORMAL) == (U64)0) vidAprdServiceDetail(APRD_PRIOR_NORMAL);
+  if ((gullAprdCounter % APRD_INTERVAL_HIGH) == (U64)0)   vidAprdServiceDetail(APRD_PRIOR_HIGH);
+}
+
+PUBLIC BOOL bAprdRegister(tenuAprdPriority enuPriority, tpfAprdInitialize pfInitialize, tpfAprdService pfService)
+{
+  U8 i;
+  BOOL bReturn = FALSE;
+
+  for (i=0; i<(U8)APRD_SRV_MAX; i++)  {
+    if (gstrAprdService[i].enuPriority != APRD_PRIOR_NA) {
+      gstrAprdService[i].enuPriority = enuPriority;
+      gstrAprdService[i].pfInitialize = pfInitialize;
+      gstrAprdService[i].pfService = pfService;
+      bReturn = TRUE;
+      break;
+    }
+  }
+  return bReturn;
 }
 
 PUBLIC void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-  static unsigned long count;
   if (htim == &htim21) {
-    if (count >= 2399) count = 0;
-    //__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, count++);
+    gullAprdCounter++;
+    vidAprdService();
+  }
+}
+
+PRIVATE void vidAprdServiceDetail(tenuAprdPriority enuAprdPriority) {
+  U8 i;
+
+  for (i=0; i<(U8)APRD_SRV_MAX; i++)  {
+    if (gstrAprdService[i].enuPriority == enuAprdPriority) {
+      if (gstrAprdService[i].pfService != NULL) {
+        gstrAprdService[i].pfService(NULL);
+      }
+    }
   }
 }
